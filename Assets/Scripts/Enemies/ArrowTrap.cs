3c61471e83cf05911580886003161466a63d2117
phatnttse <127718651+phatnttse@users.noreply.github.com>
2024-10-11 08:33:25 +0700

    Video 9 at 18p:19s

---
+++ Assets/Scripts/Enemies/ArrowTrap.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+    private float cooldownTimer;
+
+    private void Attack()
+    {
+        cooldownTimer = 0;
+
+        fireballs[FindFireBall()].transform.position = firePoint.position;
+        fireballs[FindFireBall()].GetComponent<EnemyProjectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireBall()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+        if (cooldownTimer >= attackCooldown)
+        {
+            Attack();
+        }
+    }
+
+}
